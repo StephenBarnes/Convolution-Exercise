3f21a456d1edfa3ed6a037abac1f746171af0afd
J <stbarnes@stanford.edu>
2019-09-26 05:50:41 -0700

    Implement PyCUDA convolution

--- main.py
+++ main.py
@@ -3,10 +3,11 @@
 import argparse
 
 import numpy as np
-from overlapadd2_function import overlapadd2_int16
+from overlapadd2_fn import overlapadd2_int16
+from convolve_pycuda_fn import convolve_pycuda
 
 MAX_RANDOM_INT = 255 # equal to np.iinfo(np.uint8).max
-#MAX_RANDOM_INT = 3
+MAX_RANDOM_INT = 3 # TODO remove
 
 FILTER = np.array([[-1, 0, 1]])
 
@@ -14,6 +15,9 @@ FILTER = np.array([[-1, 0, 1]])
 
 
 def convolutions_np(M, T):
+    """
+    Convolve M and T horizontally and vertically, using np.convolve.
+    """
     Dx = np.apply_along_axis(
             lambda row: np.convolve(row, T[0]),
             axis=1, arr=M)
@@ -23,14 +27,18 @@ def convolutions_np(M, T):
     return Dx, Dy
 
 def convolutions_overlap_add(M, T):
+    """
+    Convolve M and T horizontally and vertically, using overlap-add algorithm.
+    """
     Dx = overlapadd2_int16(M, T)
     Dy = overlapadd2_int16(M, T.T)
     return Dx, Dy
 
-# TODO implement PyCuda version
-
 
 def positive_int(s):
+    """
+    Convert string to int. Raise an error if string is nonpositive.
+    """
     val = int(s)
     if val <= 0:
         raise argparse.ArgumentTypeError("Argument must be positive integer")
@@ -58,11 +66,19 @@ if __name__ == "__main__":
     print(Dy)
     print()
 
-    Dx, Dy = convolutions_np(M, FILTER)
+    #Dx, Dy = convolutions_np(M, FILTER)
+
+    #print(M)
+    #print()
+    #print(Dx)
+    #print()
+    #print(Dy)
+
+
+    Dx, Dy = convolve_pycuda(M, FILTER)
 
     print(M)
     print()
     print(Dx)
     print()
     print(Dy)
-
